b967f3d48af302fa422d41bb18df51a444c2e7b5
Guillermo <guillermo@futura.es>
2023-02-08 15:47:15 +0100

    CI/CD pipelines

--- step2/src/eventsService/index.js
+++ step2/src/eventsService/index.js
@@ -5,6 +5,13 @@ const { dbConnection } = require('./database/configdb');
 // Create an Expres's application
 const app = express();
 
+
+// Consul
+const consul = require('consul');
+
+
+  
+
 dbConnection();
 
 app.use(express.json());
@@ -25,5 +32,27 @@ app.use('/', require('./routes/events'));
 app.listen(process.env.PORT, () => {
     console.log('Running in the port', process.env.PORT);
 });
+
+consul.Agent.Service.register({
+    name: 'events',
+    address: 'localhost',
+    port: 3000,
+    check: {
+      http: 'http://localhost:3000/health',
+      interval: '10s'
+    }
+  }, function(err) {
+    if (err) throw new Error(err);
+    console.log('registered with Consul');
+  });
+
+  consul.catalog.service.nodes('tickets', function(err, result) {
+    if (err) throw new Error(err);
+    console.log(result);
+  });
+
+app.get('/health', function(req, res) {
+    res.status(200).send('healthy');
+  });
 
 module.exports = app;
