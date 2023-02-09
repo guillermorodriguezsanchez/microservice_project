9aab9b2a53a05838d17c5d805abbf63f1478ed50
Guillermo <guillermo@futura.es>
2023-02-09 16:07:16 +0100

    consul done

--- step2/src/eventsService/index.js
+++ step2/src/eventsService/index.js
@@ -5,18 +5,35 @@ const { dbConnection } = require('./database/configdb');
 // Create an Expres's application
 const app = express();
 
+const Consul = require('consul');
+const consul = new Consul({host:'consul', port: '8500'});
 
-
-
-console.log("hello");
+// register consul
+async function registerService() {
+    try {
+      await consul.agent.service.register({
+        name: "eventService",
+        address: "eventService",
+        port: 3000
+      });
+      console.log('Successfully registered with Consul');
+    } catch (error) {
+      console.error('Error registering with Consul:', error);
+    }
+}
+  
 
   
 
 dbConnection();
 
+// register consul
+
+
 app.use(express.json());
 app.use(cors());
 
+registerService();
 
 app.get('/', (req,res) => {
     res.json({
@@ -29,8 +46,12 @@ app.get('/', (req,res) => {
 app.use('/', require('./routes/events'));
 //app.use('/', require('./src/ticketsService/routes/tickets'));
 
+
+
 app.listen(process.env.PORT, () => {
     console.log('Running in the port', process.env.PORT);
 });
+
+
 
 module.exports = app;
--- step2/src/ticketsService/index.js
+++ step2/src/ticketsService/index.js
@@ -5,7 +5,25 @@ const { dbConnection } = require('./database/configdb');
 // Create an Expres's application
 const app = express();
 
+const Consul = require('consul');
+const consul = new Consul({host:'consul', port : '8500'});
+// register consul
+async function registerService() {
+    try {
+      await consul.agent.service.register({
+        name: "ticketService",
+        address: "ticketService",
+        port: 3000
+      });
+      console.log('Successfully registered with Consul');
+    } catch (error) {
+      console.error('Error registering with Consul:', error);
+    }
+}
+
 dbConnection();
+
+registerService();
 
 app.use(express.json());
 app.use(cors());
